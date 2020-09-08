68598366e45b771c44f2ac6a6f800962a69e828f
Saham-Negar <70937980+Saham-Negar@users.noreply.github.com>
2020-09-08 14:06:28 +0430

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -3,6 +3,21 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
+# from tkinter import *
+# import tkinter
+# from tkinter import filedialog, Text , Label , messagebox
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import math
+# import pandas_datareader as web
+from sklearn.preprocessing import MinMaxScaler
+# from keras.models import Sequential
+# from keras.layers import Dense, LSTM
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout, GRU, RNN, SimpleRNN
 
 st.write("""
 # اپ سهام نگر 
@@ -11,48 +26,58 @@ st.write("""
 
 """)
 
-# st.sidebar.header('User Input Features')
-
+st.sidebar.header('انتخاب فایل اکسل سهام مورد نظر')
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+    st.write(input_df)
+else:
+#     def user_input_features():
+     st.write('هیچ فایلی پیدا نشد')
+      
+      
+      
 # st.sidebar.markdown("""
 # [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
 # """)
 
-# # Collects user input features into dataframe
+# Collects user input features into dataframe
 # uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 # if uploaded_file is not None:
 #     input_df = pd.read_csv(uploaded_file)
 # else:
 #     def user_input_features():
-#         island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-#         sex = st.sidebar.selectbox('Sex',('male','female'))
-#         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-#         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-#         flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-#         body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-#         data = {'island': island,
-#                 'bill_length_mm': bill_length_mm,
-#                 'bill_depth_mm': bill_depth_mm,
-#                 'flipper_length_mm': flipper_length_mm,
-#                 'body_mass_g': body_mass_g,
-#                 'sex': sex}
-#         features = pd.DataFrame(data, index=[0])
-#         return features
+# #         island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+# #         sex = st.sidebar.selectbox('Sex',('male','female'))
+# #         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+# #         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+# #         flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+# #         body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+# #         data = {'island': island,
+# #                 'bill_length_mm': bill_length_mm,
+# #                 'bill_depth_mm': bill_depth_mm,
+# #                 'flipper_length_mm': flipper_length_mm,
+# #                 'body_mass_g': body_mass_g,
+# #                 'sex': sex}
+# #         features = pd.DataFrame(data, index=[0])
+# #         return features
 #     input_df = user_input_features()
 
 # # Combines user input features with entire penguins dataset
 # # This will be useful for the encoding phase
 # penguins_raw = pd.read_csv('penguins_cleaned.csv')
-# penguins = penguins_raw.drop(columns=['species'])
-# df = pd.concat([input_df,penguins],axis=0)
+# # penguins = penguins_raw.drop(columns=['species'])
+# # df = pd.concat([input_df,penguins],axis=0)
+# df = pd.readcsv)
 
 # # Encoding of ordinal features
 # # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-# encode = ['sex','island']
-# for col in encode:
-#     dummy = pd.get_dummies(df[col], prefix=col)
-#     df = pd.concat([df,dummy], axis=1)
-#     del df[col]
-# df = df[:1] # Selects only the first row (the user input data)
+# # encode = ['sex','island']
+# # for col in encode:
+# #     dummy = pd.get_dummies(df[col], prefix=col)
+# #     df = pd.concat([df,dummy], axis=1)
+# #     del df[col]
+# # df = df[:1] # Selects only the first row (the user input data)
 
 # # Displays the user input features
 # st.subheader('User Input features')
@@ -67,13 +92,13 @@ st.write("""
 # load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
 
 # # Apply model to make predictions
-# prediction = load_clf.predict(df)
-# prediction_proba = load_clf.predict_proba(df)
+# # prediction = load_clf.predict(df)
+# # prediction_proba = load_clf.predict_proba(df)
 
 
-# st.subheader('Prediction')
-# penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-# st.write(penguins_species[prediction])
+# # st.subheader('Prediction')
+# # penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+# # st.write(penguins_species[prediction])
 
 # st.subheader('Prediction Probability')
-# st.write(prediction_proba)
+# # st.write(prediction_proba)
